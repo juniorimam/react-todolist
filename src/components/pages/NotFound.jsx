aebfe9e7cbaa55e03eacbe6a2654a70890fd6f03
juniorimam <juniorimam123@gmail.com>
2022-08-08 18:53:02 +0700

    update

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -3,7 +3,7 @@ export function NotFound() {
     <div className="flex flex-col justify-center items-center text-center h-full xs:-mt-8">
       <img src="/empty.gif" alt="Empty Todo List" className="w-96 xs:w-64" />
       <h1 className="font-bold text-4xl text-red-400 xs:text-3xl">Oops !</h1>
-      <p className="text-gray-400 mt-3 xs:text-sm">
+      <p className="text-gray-400 mt-3 xs:text-sm dark:text-neutral-500">
         You don't have any activity todo. <br />
         Are you bored? take it easy, chill on{" "}
         <a href="https://youtube.com/" target="_blank" className="text-sky-500">
